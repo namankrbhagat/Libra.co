4f96ab864bb09a21625d889b082a286bf0f3b524
namankrbhagat <namanbhagat.8161@gmail.com>
2026-02-08 00:43:34 +0530

    bugs

--- backend/lib/util.js
+++ backend/lib/util.js
@@ -9,7 +9,7 @@ export const generateToken = async (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, //MS
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
     sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
-    secure: process.env.NODE_ENV !== "development"
+    secure: process.env.NODE_ENV !== "development" ? true : false,
 
   })
 
